e49de8e7447d34841ea22de574ff949623629691
rickygeng <rickygeng97@gmail.com>
2019-11-24 12:52:35 +0800

    authentication completed

--- app/public/components/signup/signup.js
+++ app/public/components/signup/signup.js
@@ -27,29 +27,52 @@ $("#signup-form").submit(function(event) {
     }
   });
 
-  $.ajax({
-    type: "POST",
-    url: "http://localhost:5000/signup",
-    data: values,
-    dataType: "json",
-    success: function(res) {
-      if (res == "0") {
-        location.href = "../feed/feed.html";
-      } else if (res == "1") {
-        $("#email-field").css("border", "2px solid red");
-        if ($("#email-exist").length == 0) {
-          $(
-            "<div class='error' id='email-exist'>Error: Email already exists.</div>"
-          ).insertAfter("#email-field");
-        }
-      } else if (res == "2") {
-        $("#username-field").css("border", "2px solid red");
-        if ($("#username-exist").length == 0) {
-          $(
-            "<div class='error' id='username-exist'>Error: Username already exists.</div>"
-          ).insertAfter("#username-field");
+  // Check if email & password are valid.
+  if (values["email"] != "" && !validateEmail(values["email"])) {
+    $("#email-field").css("border", "2px solid red");
+    $(
+      "<div class='error' id='email-invalid'>Error: Email is invalid format.</div>"
+    ).insertAfter($("#email-field"));
+    return;
+  }
+
+  if (values["password"] != "" && !validatePassword(values["password"])) {
+    $("#password-field").css("border", "2px solid red");
+    $(
+      "<div class='error' id='password-invalid'>Error: Password is invalid format.</div>"
+    ).insertAfter($("#password-field"));
+    return;
+  }
+
+  if (
+    values["email"] != "" &&
+    values["username"] != "" &&
+    values["password"] != ""
+  ) {
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:5000/signup",
+      data: values,
+      dataType: "json",
+      success: function(res) {
+        if (res == "0") {
+          location.href = "../feed/feed.html";
+        } else if (res == "1") {
+          $("#email-field").css("border", "2px solid red");
+          if ($("#email-exist").length == 0) {
+            $(
+              "<div class='error' id='email-exist'>Error: Email already exists.</div>"
+            ).insertAfter("#email-field");
+          }
+        } else if (res == "2") {
+          $("#username-field").css("border", "2px solid red");
+          if ($("#username-exist").length == 0) {
+            $(
+              "<div class='error' id='username-exist'>Error: Username already exists.</div>"
+            ).insertAfter("#username-field");
+          }
         }
       }
-    }
-  });
+    });
+  }
 });
---
+++ app/public/utils/utils.js
@@ -0,0 +1,9 @@
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function validatePassword(password) {
+  var re = /^[0-9a-zA-Z]{8,}$/;
+  return re.test(String(password));
+}
